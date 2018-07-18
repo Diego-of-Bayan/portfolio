14cc7b690a8b477431edfc5c95902e5bfab23895
Diego Ramil <dbramil811@gmail.com>
2018-07-18 20:54:09 +0000

    added scroll animation

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 /*global $*/
-$(".secondary").hide();
+$(".mainMain").hide();
 $(".rowMid2").hide();
+$(".mid").css("opacity","1");
+var scrollCount = 0;
 setTimeout(function(){
-    $(".mid").hide();
-},3000);
+    $(".mid").css("transform", "translate(0px,-250px) scale(0.5)");
+},2000);
+window.onscroll = function() {
+    if (scrollCount === 0) {
+        scrollCount ++;
+        $(".mainMain").show();
+        $(".aboutme").hide();
+        $(".contact").hide();
+        $(".mid").css("opacity","0.5");
+    } else if (scrollCount === 1){
+        console.log("scroll1");
+        scrollCount ++;
+        $(".aboutme").show();
+    } else if (scrollCount === 2) {
+        scrollCount ++;
+        $(".contact").show();
+    }
+};
